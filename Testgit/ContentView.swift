fbf054d935be9b64e48492db86b5c5cf56a4adfa
tosky2023 <panhequn007@gmail.com>
2023-07-31 14:30:21 +0800

    test merge

--- Testgit/ContentView.swift
+++ Testgit/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Test GIT")
+            Text("text marge")
         }
         .padding()
     }
