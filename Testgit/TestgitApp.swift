ecbb2bb986957589e949ebb3a2a0dfa1ec91c320
tosky2023 <panhequn007@gmail.com>
2023-07-31 14:08:49 +0800

    Initial Commit

---
+++ Testgit/TestgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestgitApp.swift
+//  Testgit
+//
+//  Created by 潘鹤群 on 2023/7/31.
+//
+
+import SwiftUI
+
+@main
+struct TestgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
